fa4ef6d8c57343539c990a4bdd842a2010e1671d
dhananjay1002000 <55107701+dhananjay1002000@users.noreply.github.com>
2025-08-06 17:30:29 +0530

    Update main.py

--- main.py
+++ main.py
@@ -121,5 +121,5 @@ def read_root():
 if __name__ == "__main__":
     import uvicorn
     port = int(os.environ.get("PORT", 8000))
-    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=port)
 
